cc0090c1e353492db5d03fb09a1ca2c8e08bbe53
messhii <messhiidada@gmail.com>
2023-11-30 23:03:04 +0900

    first commit

---
+++ Bit/ABC167_C.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    int n, m, x;
+    cin >> n >> m >> x;
+    vector<vector<int>> a(n, vector<int>(m));
+    vector<int> price(n);
+    for(int i=0; i<n; i++){
+        cin >> price[i];
+        for(int& e: a[i]) cin >> e;
+    }
+    int ans = 1e9;
+    for(int bit=0; bit < (1<<n); bit++){
+        vector<int> sum(m);
+        int total = 0;
+        for(int i=0; i<n; i++){
+            if(bit & 1<<i){
+                total += price[i];
+                for(int j=0; j<m; j++){
+                    sum[j] += a[i][j];
+                }
+            }
+        }
+        int cnt = 0;
+        for(int e: sum) if(e >= x) cnt++;
+        if(cnt == m) ans = min(ans, total);
+    }
+    if(ans == 1e9) ans = -1;
+    cout << ans << endl;
+    return 0;
+}
