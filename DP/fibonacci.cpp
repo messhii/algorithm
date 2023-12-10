4a86ef7a3820f8a76eb4001f2ade2528ec7b9377
messhii <messhiidada@gmail.com>
2023-12-10 20:04:45 +0900

    Update

--- DP/fibonacci.cpp
+++ DP/fibonacci.cpp
@@ -6,9 +6,9 @@ int main(){
     int n;
     cin >> n;
     vector<int> dp(n);
-    for(int i=1; i<n; i++){
-        if(i <= 2) dp[i] = 1;
-        else dp[i] = dp[i-1] + dp[i-2];
+    dp[0] = dp[1] = 1;
+    for(int i=2; i<n; i++){
+        dp[i] = dp[i-1] + dp[i-2];
     }
     for(int a: dp) cout << a << " ";
     cout << endl;
