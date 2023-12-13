d83ede361c2a97332f10609986bad0017163d58c
messhii <messhiidada@gmail.com>
2023-12-13 23:42:34 +0900

    first commit

--- DP/EDPC_C.cpp
+++ DP/EDPC_C.cpp
@@ -8,9 +8,9 @@ int main(){
     vector<int> a(n+1), b(n+1), c(n+1);
     vector<vector<int>> dp(n+1, vector<int>(3));
     for(int i=1; i<=n; i++) cin >> a[i] >> b[i] >> c[i];
-    dp[1][0] = a[1], dp[1][1] = b[1], dp[1][2] = c[1];
-
-    for(int i=2; i<=n; i++){
+    
+    dp[0][0] = dp[0][1] = dp[0][2] = 0;
+    for(int i=1; i<=n; i++){
         dp[i][0] = a[i] + max(dp[i-1][1], dp[i-1][2]);
         dp[i][1] = b[i] + max(dp[i-1][0], dp[i-1][2]);
         dp[i][2] = c[i] + max(dp[i-1][0], dp[i-1][1]);
