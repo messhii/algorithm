9dd666b93fd7a854ebcebce5d6cfe34e53a89826
messhii <messhiidada@gmail.com>
2023-12-20 23:03:32 +0900

    Update

--- Bit/ABC249_C.cpp
+++ Bit/ABC249_C.cpp
@@ -6,21 +6,21 @@ int main(){
     int n, k;
     cin >> n >> k;
     vector<string> s(n);
-    for(auto& s2: s) cin >> s2;
+    for(string& e: s) cin >> e;
 
     int ans = 0;
-    for(int bit=0; bit < (1<<n); bit++){
-        int alpha_cnt[26] = {};
+    for(int bit=0; bit<(1<<n); bit++){
+        vector<int> cnt(26);
         for(int i=0; i<n; i++){
             if(bit & (1<<i)){
-                for(auto e: s[i]) alpha_cnt[e - 'a']++;
+                for(char c: s[i]) cnt[c - 'a']++;
             }
         }
-        int cnt = 0;
-        for(int i=0; i<26; i++){
-            if(alpha_cnt[i] == k) cnt++;
+        int k_cnt = 0;
+        for(int e: cnt){
+            if(e == k) k_cnt++;
         }
-        ans = max(ans, cnt);
+        ans = max(ans, k_cnt);
     }
     cout << ans << endl;
     return 0;
